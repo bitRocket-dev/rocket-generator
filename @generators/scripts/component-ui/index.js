4c26e0ffbcffac39402311c8f9ac833eeaf63555
Daniele <daniele@bitrocket.dev>
2021-06-09 17:35:10 +0200

    feat(general): add dynamic path utility

--- @generators/scripts/component-ui/index.js
+++ @generators/scripts/component-ui/index.js
@@ -2,20 +2,22 @@
 
 const fs = require("fs-extra");
 const { component, story } = require("./templates.js");
-const { utilityCapitalizeFirst } = require("../../utilities.js");
+const { utilityCapitalizeFirst, utilityPath } = require("../../utilities.js");
 
 const [name] = process.argv.slice(2);
 if (!name) throw new Error("You must include a component name.");
 
 const formattedName = utilityCapitalizeFirst(name);
 const dir = `./src/components-ui/${formattedName}`;
+const path = "./src/components-ui";
 
 const localDir = `${__dirname}/${formattedName}`;
 
 if (fs.existsSync(dir))
-  throw new Error("A component with that name already exists.");
+  console.error(`\x1b[31m`, "A component with that name already exists.");
 
 if (fs.existsSync(localDir)) return fs.copy(localDir, dir);
+else utilityPath(path);
 
 fs.mkdirSync(dir);
 
--- @generators/utilities.js
+++ @generators/utilities.js
@@ -1,4 +1,18 @@
 /** @format */
+const fs = require("fs-extra");
 
-exports.utilityCapitalizeFirst = string =>
+exports.utilityCapitalizeFirst = (string) =>
   string.toLowerCase().charAt(0).toUpperCase() + string.slice(1);
+
+exports.utilityPath = (string) => {
+  let str = ".";
+  const arr = string
+    .split("/")
+    .filter((item) => item != ".")
+    .map((item) => {
+      str += "/" + item;
+      if (!fs.existsSync(str)) {
+        fs.mkdirSync(str);
+      }
+    });
+};
