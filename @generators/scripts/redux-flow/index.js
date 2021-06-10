a8997a2641389c4bc0eaf2759d8d7d2b892c3b5a
Vincenzo De Sisto <vincenzo@bitrocket.dev>
2021-06-10 11:07:14 +0200

    fix(redux-flow): fix mkdir to mkdirs in index.js

--- @generators/scripts/redux-flow/index.js
+++ @generators/scripts/redux-flow/index.js
@@ -1,21 +1,22 @@
 /** @format */
 
-const fs = require('fs');
-const { sagas } = require('./templates/sagas');
-const { actions } = require('./templates/actions');
-const { constants } = require('./templates/constants');
-const { api } = require('./templates/api');
-const { declarations } = require('./templates/declarations');
-const { reducers } = require('./templates/reducers');
+const fs = require("fs-extra");
+const { sagas } = require("./templates/sagas");
+const { actions } = require("./templates/actions");
+const { constants } = require("./templates/constants");
+const { api } = require("./templates/api");
+const { declarations } = require("./templates/declarations");
+const { reducers } = require("./templates/reducers");
 
 const [name] = process.argv.slice(2);
-if (!name) throw new Error('You must include a component name.');
+if (!name) throw new Error("You must include a component name.");
 
 const dir = `./src/@ducks/${name}`;
 
-if (fs.existsSync(dir)) throw new Error('A component with that name already exists.');
+if (fs.existsSync(dir))
+  throw new Error("A component with that name already exists.");
 
-fs.mkdirSync(dir);
+fs.mkdirsSync(dir);
 
 function writeFileErrorHandler(err) {
   if (err) throw err;
@@ -24,6 +25,10 @@ function writeFileErrorHandler(err) {
 fs.writeFile(`${dir}/sagas.tsx`, sagas(name), writeFileErrorHandler);
 fs.writeFile(`${dir}/actions.tsx`, actions(name), writeFileErrorHandler);
 fs.writeFile(`${dir}/constants.tsx`, constants(name), writeFileErrorHandler);
-fs.writeFile(`${dir}/declarations.tsx`, declarations(name), writeFileErrorHandler);
+fs.writeFile(
+  `${dir}/declarations.tsx`,
+  declarations(name),
+  writeFileErrorHandler
+);
 fs.writeFile(`${dir}/api.tsx`, api(name), writeFileErrorHandler);
 fs.writeFile(`${dir}/reducers.tsx`, reducers(name), writeFileErrorHandler);
