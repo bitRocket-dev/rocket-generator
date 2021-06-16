7239741b19d97b2b4a2b44c82a6d3b03ae3d5838
Gabriele <gabriele@bitrocket.dev>
2021-06-16 16:14:18 +0200

    fix(general): test and fix cypress components

--- @generators/scripts/component-ui/templates.js
+++ @generators/scripts/component-ui/templates.js
@@ -37,7 +37,7 @@ Default.args = {};
 exports.test = (name) => `
 import { UI${name} } from '.';
 import { mount } from '@cypress/react'
-import { cyGet } from "../cypress/support/commands";
+import { cyGet } from "../../../cypress/support/commands";
 
 it('renders learn react link', () => {
   mount(<UI${name} />)
--- @generators/scripts/component-view/templates.js
+++ @generators/scripts/component-view/templates.js
@@ -13,7 +13,7 @@ export const View${name} :FC<Props> = ({}): JSX.Element => {
 exports.test = (name) => `
 import { View${name} } from '.';
 import { mount } from '@cypress/react'
-import { cyGet } from "../cypress/support/commands";
+import { cyGet } from "../../../cypress/support/commands";
 
 it('renders learn react link', () => {
   mount(<View${name} />)
