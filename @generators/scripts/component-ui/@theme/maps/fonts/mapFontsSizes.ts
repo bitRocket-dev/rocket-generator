d6e1e4839d4db402b0e11a77d6d4f83f16e9c5f0
Roberto Portluri <roberto@bitrocket.dev>
2021-06-17 16:44:01 +0200

    fix(general): fix variantfont

--- @generators/scripts/component-ui/@theme/maps/fonts/mapFontsSizes.ts
+++ @generators/scripts/component-ui/@theme/maps/fonts/mapFontsSizes.ts
@@ -1,8 +1,20 @@
 /** @format */
 
-export interface TFontSizeValues {}
+export interface TFontSizeValues {
+  display: string;
+  header: string;
+  title: string;
+  subtitle: string;
+  caption: string;
+}
 
-export const mapFontsSize: TFontSizeValues = {};
+export const mapFontsSize: TFontSizeValues = {
+  display: "50px",
+  header: "28px",
+  title: "21px",
+  subtitle: "16px",
+  caption: "14px",
+};
 
 export type TFontVariant = keyof TFontSizeValues;
 export type TFontSize = keyof TFontSizeValues;
--- @generators/scripts/component-ui/@theme/maps/fonts/mapFontsWeights.ts
+++ @generators/scripts/component-ui/@theme/maps/fonts/mapFontsWeights.ts
@@ -1,7 +1,17 @@
 /** @format */
 
-export interface TFontWeightValues {}
+export interface TFontWeightValues {
+  regular: string;
+  medium: string;
+  bold: string;
+  black: string;
+}
 
 export type TFontWeight = keyof TFontWeightValues;
 
-export const mapFontsWeights: TFontWeightValues = {};
+export const mapFontsWeights: TFontWeightValues = {
+  regular: '400',
+  medium: '500',
+  bold: '700',
+  black: '900',
+};
