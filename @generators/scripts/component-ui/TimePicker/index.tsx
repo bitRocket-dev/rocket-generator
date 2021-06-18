41e0c3a01619a0e9c0001644e61cd674927d7767
Roberto Portluri <roberto@bitrocket.dev>
2021-06-18 10:07:19 +0200

    fix(general): fix time picker comp ui

--- @generators/scripts/component-ui/TimePicker/index.tsx
+++ @generators/scripts/component-ui/TimePicker/index.tsx
@@ -1,11 +1,11 @@
 /** @format */
 
 // #region ::: IMPORT
-import { FC, memo, useState } from "react";
-import DatePicker from "react-datepicker";
-import { Field } from "./partials/Field";
-import { UIText } from "../Text";
-import { InputDate } from "../Input/Date";
+import { FC, memo, useState } from 'react';
+import DatePicker from 'react-datepicker';
+import { Field } from './partials/Field';
+import { UIText } from '../Text';
+import { InputDate } from '../Input/Date';
 
 // #endregion
 
@@ -21,9 +21,9 @@ const TimePickerCustomInput = memo(
       {label && <UIText text={label} variant="caption" />}
       <InputDate onClick={onClick} value={value} icon="time" isFilter={false} />
     </Field>
-  )
+  ),
 );
-TimePickerCustomInput.displayName = "TimePickerCustomInput";
+TimePickerCustomInput.displayName = 'TimePickerCustomInput';
 
 export interface Props {
   label?: string;
@@ -32,33 +32,31 @@ export interface Props {
   fluid?: boolean;
 }
 
-export const UITimePicker: FC<Props> = memo(
-  ({ label, value, onChange, fluid = false }: Props): JSX.Element => {
-    const initialState = value ? value : new Date();
-    const [startDate, setStartDate] = useState(initialState);
+export const UITimePicker: FC<Props> = memo(({ label, value, onChange, fluid = false }: Props): JSX.Element => {
+  const initialState = value ? value : new Date();
+  const [startDate, setStartDate] = useState(initialState);
 
-    const handleChange = (date: Date | null): void => {
-      if (date) {
-        setStartDate(date);
-      }
-      onChange(date);
-    };
+  const handleChange = (date: Date | null): void => {
+    if (date) {
+      setStartDate(date);
+    }
+    onChange(date);
+  };
 
-    return (
-      <DatePicker
-        wrapperClassName={fluid ? "wrapper" : ""}
-        locale="it"
-        dateFormat="RocketBoilerplate:mm"
-        showTimeSelect
-        showTimeSelectOnly
-        timeIntervals={30}
-        timeCaption="Ora"
-        selected={startDate}
-        onChange={handleChange}
-        customInput={<TimePickerCustomInput label={label} />}
-      />
-    );
-  }
-);
+  return (
+    <DatePicker
+      wrapperClassName={fluid ? 'wrapper' : ''}
+      locale="it"
+      dateFormat="HH:mm"
+      showTimeSelect
+      showTimeSelectOnly
+      timeIntervals={30}
+      timeCaption="Ora"
+      selected={startDate}
+      onChange={handleChange}
+      customInput={<TimePickerCustomInput label={label} />}
+    />
+  );
+});
 
-UITimePicker.displayName = "UITimePicker";
+UITimePicker.displayName = 'UITimePicker';
