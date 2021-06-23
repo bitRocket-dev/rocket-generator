5167a51fee0dc8af6d3cc8c843e61381123a374a
Vincenzo De Sisto <vincenzo@bitrocket.dev>
2021-06-23 16:18:29 +0200

    fix(general): wip

--- @generators/scripts/sync-flow/index.js
+++ @generators/scripts/sync-flow/index.js
@@ -8,23 +8,31 @@ const { reducers } = require("./templates/reducers");
 
 async function reduxSyncFlow(name, choices, reducer) {
   const dir = `./src/@sdk/redux-modules/${name}`;
+  const dir2 = `./src/@sdk/redux-modules/${name}/actions.tsx`;
 
-  if (await fs.pathExists(dir))
-    console.error(
-      `\x1b[31m`,
-      `A component ${name.toUpperCase()} already exists.`
+  if (await fs.pathExists(dir2)) {
+    const data = fs
+      .readFileSync(`./src/@sdk/redux-modules/${name}/actions.tsx`)
+      .toString()
+      .split("\n");
+    console.log(data.length);
+    data.splice(4, 0, "Test prova");
+    const text = data.join("\n");
+    fs.writeFile(`${dir2}`, text, function (err) {
+      if (err) return console.log(err);
+    });
+  } else {
+    await fs.mkdirs(dir);
+    fs.writeFile(
+      `${dir}/actions.tsx`,
+      actions(name, choices),
+      writeFileErrorHandler
     );
-  else await fs.mkdirs(dir);
+  }
 
   function writeFileErrorHandler(err) {
     if (err) throw err;
   }
-
-  fs.writeFile(
-    `${dir}/actions.tsx`,
-    actions(name, choices),
-    writeFileErrorHandler
-  );
 
   fs.writeFile(
     `${dir}/constants.tsx`,
