d19349e9b90bcc39202b6d3294b206a09561cdab
Riccardo Genova <riccardo@bitrocket.dev>
2021-06-24 17:32:27 +0200

    fix(general): fix import fs

--- src/@generators/scripts/packages/index.ts
+++ src/@generators/scripts/packages/index.ts
@@ -1,14 +1,14 @@
 /** @format */
 
-import fs from 'fs-extra';
+import { pathExists, copy } from 'fs-extra';
 import { throwIfError } from '../../utilities';
 
 export const packages = async name => {
   const localDir = `${__dirname}/templates/${name}`;
   const dir = `./src/@packages/${name}`;
 
-  if (await fs.pathExists(dir)) console.error(`\x1b[31m`, `A component ${name} already exists.`);
+  if (await pathExists(dir)) console.error(`\x1b[31m`, `A component ${name} already exists.`);
 
-  if (await fs.pathExists(localDir)) return fs.copy(localDir, dir).catch(() => {});
+  if (await pathExists(localDir)) return copy(localDir, dir).catch(() => {});
   throwIfError;
 };
