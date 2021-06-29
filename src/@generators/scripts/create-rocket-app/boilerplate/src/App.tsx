918ec9502a509dd1091a9cda3cb568c76a468867
Daniele <daniele@bitrocket.dev>
2021-06-29 10:04:13 +0200

    fix(general): fix export viewhome and viewlogin

--- src/@generators/scripts/create-rocket-app/boilerplate/src/App.tsx
+++ src/@generators/scripts/create-rocket-app/boilerplate/src/App.tsx
@@ -1,11 +1,19 @@
 /** @format */
 
 import { FC } from 'react';
+import { BrowserRouter, Switch } from 'react-router-dom';
+import { RouteLogged } from './components-routing/RouteLogged';
+import { RouteGuest } from './components-routing/RouteGuest';
+import { ViewHome } from './components-view/ViewHome';
+import { ViewLogin } from './components-view/ViewLogin';
 
 export const App: FC = (): JSX.Element => {
   return (
-    <header>
-      <h5>ROCKET APP GENERATOR!</h5>
-    </header>
+    <BrowserRouter>
+      <Switch>
+        <RouteGuest path="/login" component={ViewLogin} />
+        <RouteLogged exact path="/" component={ViewHome} />
+      </Switch>
+    </BrowserRouter>
   );
 };
